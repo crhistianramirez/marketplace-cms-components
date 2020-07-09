26689625b0e6a2564f9d1e11adb500a1e0613e30
Crhistian Ramirez <cramirez@four51.com>
2020-07-09 14:01:00 -0500

    :sparkles: use storybook

--- src/storybook/html-editor.stories.ts
+++ src/storybook/html-editor.stories.ts
@@ -2,6 +2,13 @@ import { moduleMetadata } from '@storybook/angular';
 import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
 import { HtmlEditorComponent } from 'src/app/components/html-editor/html-editor.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SectionPickerComponent } from 'src/app/components/section-picker/section-picker.component';
+import { CarouselEditorComponent } from 'src/app/components/carousel-editor/carousel-editor.component';
+import { SectionDateSettingsComponent } from 'src/app/components/section-date-settings/section-date-settings.component';
+import { ConfirmModalComponent } from 'src/app/components/confirm-modal/confirm-modal.component';
+import { SectionTemplateRendererComponent } from 'src/app/components/section-template-renderer/section-template-renderer.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export default {
   title: 'HTML Editor',
@@ -11,12 +18,28 @@ export default {
   },
   decorators: [
     moduleMetadata({
-      declarations: [],
-      imports: [EditorModule, NgbModule],
-      providers: [
-        { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }
+      declarations: [
+        SectionPickerComponent, 
+        CarouselEditorComponent,
+        SectionDateSettingsComponent,
+        ConfirmModalComponent,
+        SectionTemplateRendererComponent
       ],
-      entryComponents: []
+      imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        EditorModule, 
+        NgbModule
+      ],
+      providers: [
+        { provide: TINYMCE_SCRIPT_SRC, useValue: './plugin/dist/plugin.min.js' }
+      ],
+      entryComponents: [
+        SectionPickerComponent, 
+        CarouselEditorComponent,
+        SectionDateSettingsComponent,
+        ConfirmModalComponent
+      ]
     })
   ]
 };
@@ -25,34 +48,19 @@ export const BasicExample = () => ({
   component: HtmlEditorComponent,
   props: {
     ocToken: '',
-    initialValue: '',
-    // remoteCss:
-    //   'https://mgrstoragetest.azureedge.net/buyerweb/styles.e94215343d3493186ae1.css'
-    content_css: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css',
-  }
-});
-
-export const ThemeExample = () => ({
-  component: HtmlEditorComponent,
-  props: {
-    ocToken: '',
-    initialValue: `
-      <h1>h1. Bootstrap heading</h1>
-      <h2>h2. Bootstrap heading</h2>
-      <h3>h3. Bootstrap heading</h3>
-      <h4>h4. Bootstrap heading</h4>
-      <h5>h5. Bootstrap heading</h5>
-      <h6>h6. Bootstrap heading</h6>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
-      <button type="button" class="btn btn-success">Success</button>
-      <button type="button" class="btn btn-danger">Danger</button>
-      <button type="button" class="btn btn-warning">Warning</button>
-      <button type="button" class="btn btn-info">Info</button>
-      <button type="button" class="btn btn-light">Light</button>
-      <button type="button" class="btn btn-dark">Dark</button>
-    `,
-    remoteCss:
-      'https://mgrstoragetest.azureedge.net/buyerweb/styles.e94215343d3493186ae1.css'
-  }
+    editorOptions: {
+      ordercloud: {
+        marketplaceUrl: 'https://marketplace-middleware-test.azurewebsites.net',
+        access_token:
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c3IiOiJhbm9uLXRlbXBsYXRlLXVzZXIiLCJjaWQiOiJlOTU1ZDZlYy01OWM4LTQ0NDItOTQ4ZS0xNmNjYzVmZDI3YWUiLCJvcmRlcmlkIjoiWTR5SzNwdUl1MGE3Qk5CV2pjWEVMQSIsInUiOiIxMDIwMjMwIiwidXNydHlwZSI6ImJ1eWVyIiwicm9sZSI6WyJNZUFkZHJlc3NBZG1pbiIsIk1lQWRtaW4iLCJNZUNyZWRpdENhcmRBZG1pbiIsIk1lWHBBZG1pbiIsIlNob3BwZXIiLCJTdXBwbGllclJlYWRlciIsIlN1cHBsaWVyQWRkcmVzc1JlYWRlciIsIlBhc3N3b3JkUmVzZXQiLCJCdXllclJlYWRlciJdLCJpc3MiOiJodHRwczovL2F1dGgub3JkZXJjbG91ZC5pbyIsImF1ZCI6Imh0dHBzOi8vYXBpLm9yZGVyY2xvdWQuaW8iLCJleHAiOjE1OTQ4NDUxNTksIm5iZiI6MTU5NDI0MDM1OX0.lpQBQAYqlssXmxtcHLxck5N6CGxr31ZA71SUqyxxLJE'
+      },
+      content_css: [
+        // 'https://piasstorageprod.azureedge.net/buyerweb/styles.07d24b25eb6a60350a70.css',
+        'https://mgrstoragetest.azureedge.net/buyerweb/styles.e94215343d3493186ae1.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css'
+      ]
+    }
+  },
+  
 });
