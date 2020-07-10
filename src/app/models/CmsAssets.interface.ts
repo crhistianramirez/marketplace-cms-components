0a263273a8998c08ed902d1296641533448ba50b
Crhistian Ramirez <cramirez@four51.com>
2020-07-10 11:53:25 -0500

    convert to library project, separate into buyer/admin modules

--- src/app/models/CmsAssets.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface CmsAsset {
-  url: string;
-  title: string;
-}
--- src/app/models/RowContentDoc.interface.ts
+++
@@ -1,6 +0,0 @@
-import { ContentDoc } from './ContentDoc.interface';
-import { ColumnContentDoc } from './ColumnContentDoc.interface';
-
-export interface RowContentDoc extends ContentDoc {
-    Content: ColumnContentDoc[]
-}
